72e3b5b72d6573105d4194d59dca4ef8b18e8ca6
hoyea <asas536112@gmail.com>
2017-09-30 14:07:56 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int n;
+	double n2;
+	n=in.nextInt();
+	n2=n*1.6;
+	System.out.printf("%.1f\n",n2);
+    }
+}
